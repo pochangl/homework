16048d1bcdaa48621bc140fdfaf50c0cb5cdeacb
Pochang Lee <pochanglee2008@gmail.com>
2023-06-08 06:09:10 +0800

    add indentation error

---
+++ practices/error2/tests.py
@@ -0,0 +1,14 @@
+'''
+練習寫個會產生 IndentError 的程式碼
+'''
+from unittest import TestCase
+class Test(TestCase):
+    def test_case_1(self):
+        try:
+            from . import main
+        except IndentationError:
+            pass
+        except Exception as error:
+            self.fail(f'{type(error)} 不是 IndentationError')
+        else:
+            self.fail('沒有出現 IndentationError')
