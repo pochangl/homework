664df22c554d50d8e63e3987a859e42a3a42ae4a
Pochang Lee <pochanglee2008@gmail.com>
2023-06-08 05:51:33 +0800

    keyword argument practice

---
+++ practices/function3/tests.py
@@ -0,0 +1,20 @@
+'''
+練習寫個函式 absolute
+輸入: 1 個整數
+回傳: 此整數絕對值
+'''
+from unittest import TestCase
+from .main import minus
+
+class Test(TestCase):
+    def test_case_1(self):
+        result = minus(1, 2)
+        self.assertEqual(result, -1)
+
+    def test_case_2(self):
+        result = minus(a=1, b=2)
+        self.assertEqual(result, -1)
+
+    def test_case_3(self):
+        result = minus(b=2, a=1)
+        self.assertEqual(result, -1)
