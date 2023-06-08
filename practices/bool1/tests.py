3d93b4fec9d32bb32b6929d985b8c9c351f27472
Pochang Lee <pochanglee2008@gmail.com>
2023-06-08 08:15:31 +0800

    feat: add example

--- practices/bool1/tests.py
+++ practices/bool1/tests.py
@@ -10,81 +10,81 @@ class Test(TestCase):
             a = 1
         else:
             a = 2
-        self.assertEqual(a, )
+        self.assertEqual(a, 2)
 
     def test_case_string_2(self):
         if 'False':
             a = 1
         else:
             a = 2
-        self.assertEqual(a, )
+        self.assertEqual(a, 1)
 
     def test_case_list_1(self):
         if []:
             a = 1
         else:
             a = 2
-        self.assertEqual(a, )
+        self.assertEqual(a, 2)
 
     def test_case_list_2(self):
         if [False]:
             a = 1
         else:
             a = 2
-        self.assertEqual(a, )
+        self.assertEqual(a, 1)
 
     def test_case_dict_1(self):
         if {}:
             a = 1
         else:
             a = 2
-        self.assertEqual(a, )
+        self.assertEqual(a, 2)
 
     def test_case_dict_2(self):
         if {False, False}:
             a = 1
         else:
             a = 2
-        self.assertEqual(a, )
+        self.assertEqual(a, 1)
 
     def test_case_bool_1(self):
         if False:
             a = 1
         else:
             a = 2
-        self.assertEqual(a, )
+        self.assertEqual(a, 2)
 
     def test_case_bool_2(self):
         if True:
             a = 1
         else:
             a = 2
-        self.assertEqual(a, )
+        self.assertEqual(a, 1)
 
     def test_case_int_1(self):
         if 0:
             a = 1
         else:
             a = 2
-        self.assertEqual(a, )
+        self.assertEqual(a, 2)
 
     def test_case_int_2(self):
         if 1:
             a = 1
         else:
             a = 2
-        self.assertEqual(a, )
+        self.assertEqual(a, 1)
 
     def test_case_int_3(self):
         if -1:
             a = 1
         else:
             a = 2
-        self.assertEqual(a, )
+        self.assertEqual(a, 1)
 
     def test_case_int_4(self):
         if 9:
             a = 1
         else:
             a = 2
-        self.assertEqual(a, )
+        self.assertEqual(a, 1)
